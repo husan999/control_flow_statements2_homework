5a111bdd175a920aa148fa9a9cf0c06a96db887c
husan999 <husanshunqorov07@gmail.com>
2023-03-08 23:11:14 +0500

    good

--- if07.py
+++ if07.py
@@ -13,4 +13,21 @@ def main(temp):
     Returns:
         str: return answer.
     """
-    return
+    if temp < 0:
+        answer = "Freezing"
+
+    elif 1 <= temp <= 10:
+        answer = "Very Cold"
+
+    elif 11 <= temp <= 20:
+        answer = "Cold"
+    elif 21 <= temp <= 30:
+        answer = "Normal"
+    elif 31 <= temp <= 40:
+        answer = "Hot"
+    elif temp > 40:
+        answer = "Very Hot" 
+
+    return answer
+
+print(main(-87))
