666a491b47ef54fd263bbe79725acfea710a2f37
husan999 <husanshunqorov07@gmail.com>
2023-03-09 09:38:23 +0500

    good

--- if03.py
+++ if03.py
@@ -9,16 +9,11 @@ def main(a,b,c):
         int: return answer.
     """
     if (a>b) or (a>c):
-        answer=a
-    elif (a>b) and (c>a) or (c>b):
-        answer=b
-    elif (a<b) and (c<a) or (c<b):
-        answer=b
+        return a
+    elif (a>b) and (c>a) :
+        return b
+    elif (a<b) and (c<a):
+        return b
     elif a<b or a<c:
-        answer=a
-    elif a>c and c>b or a<b:
-        answer=c
-    elif a<c and c<b or a>b:
-        answer=c
-    return answer
-print(main(5,7,3))
+        return a
+print(main(6,7,3))
